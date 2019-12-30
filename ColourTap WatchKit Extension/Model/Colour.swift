8a21a9c7391bce810db126b3cc76e76f0b5a07ce
Joseph Akinde-peters <apjoex@gmail.com>
2019-12-30 10:49:10 +0100

    Minor code improvements

--- ColourTap WatchKit Extension/Model/Colour.swift
+++ ColourTap WatchKit Extension/Model/Colour.swift
@@ -10,10 +10,10 @@ import Foundation
 import UIKit
 
 struct Colour {
-    var red: CGFloat
-    var green: CGFloat
-    var blue: CGFloat
-    var name: String
+    let red: CGFloat
+    let green: CGFloat
+    let blue: CGFloat
+    let name: String
     
     var output: UIColor {
         UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
